cc38a93b7bd1fda59e82785a46eea4fcc4bcdc2c
XDurango2 <130610928+XDurango2@users.noreply.github.com>
2024-09-28 19:33:57 -0700

    update TTS
    se añadieron archivos de prueba al sistema TTS

--- TTS_local.py
+++ TTS_local.py
@@ -8,8 +8,10 @@ def run(text):
     engine.stop()
 
 def run_and_save(text):
+    engine = pyttsx3.init()  # Object creation
     """Saving Voice to a file"""
     # On linux make sure that 'espeak' and 'ffmpeg' are installed
     engine.save_to_file(text, 'text.mp3')
     engine.runAndWait()
 
+run_and_save("My key is ready to go to make requests and get speech from text from Google. I tried these commands and many more.")
