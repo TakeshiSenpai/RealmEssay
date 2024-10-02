965ba0a38b9fa520738df7867bf3cdd7f53e0744
Leonardo Larrañaga <leonardolarranaga@icloud.com>
2024-10-01 20:12:03 -0700

    Python requirements.txt, actualización de .gitignore, uso de async/await y JS sin ;

--- king-frontend/src/components/GoogleButton.js
+++ king-frontend/src/components/GoogleButton.js
@@ -1,41 +1,42 @@
-import React from 'react';
-import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
-import Button from '@mui/material/Button';
-import GoogleIcon from '@mui/icons-material/Google';
-import Typography from '@mui/material/Typography';
-import { useNavigate } from 'react-router-dom';  
+import React from 'react'
+import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google'
+import Button from '@mui/material/Button'
+import GoogleIcon from '@mui/icons-material/Google'
+import Typography from '@mui/material/Typography'
+import { useNavigate } from 'react-router-dom'  
 
 export const GoogleButton = () => {
-  const navigate = useNavigate(); 
+  const navigate = useNavigate()
 
-  const handleLoginSuccess = (credentialResponse) => {
-    const token = credentialResponse.credential; 
+  const handleLoginSuccess = async (credentialResponse) => {
+      const token = credentialResponse.credential
 
-    fetch('http://localhost:5000/login/google', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ token: token })  
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.success) {
-            localStorage.setItem('token', data.token);
-            localStorage.setItem('userInfo', JSON.stringify(data.user_info));
-            navigate('/'); 
-        } else {
-            console.log('Login failed:', data.message);
-        }
-    })
-    .catch(error => {
-        console.error('Error during login:', error);
-    });
-  };
+      try {
+          const response = await fetch('http://127.0.0.1:5000/login/google', {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({token: token})
+          })
+
+          const data = await response.json()
+
+          if (data.success) {
+              localStorage.setItem('token', data.token)
+              localStorage.setItem('userInfo', JSON.stringify(data.user_info))
+              navigate('/')
+          } else {
+              console.log('Login failed:', data.message)
+          }
+      } catch (error) {
+          console.error('Error during login:', error)
+      }
+  }
 
   const handleLoginFailure = () => {
-    console.log('Login failed.');
-  };
+    console.log('Login failed.')
+  }
 
   return (
     <GoogleOAuthProvider clientId="496696206304-fsqr77k8ao63rv6tuskh5lu4ph5p4fo3.apps.googleusercontent.com">
@@ -62,5 +63,5 @@ export const GoogleButton = () => {
         />
       </div>
     </GoogleOAuthProvider>
-  );
-};
+  )
+}
