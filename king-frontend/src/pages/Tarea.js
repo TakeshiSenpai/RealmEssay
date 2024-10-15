1ef27def0ba9caa85a349722c617a14ca2bb44ea
XDurango2 <130610928+XDurango2@users.noreply.github.com>
2024-10-15 14:11:56 -0700

    Listo el cambio de vista y practicamente listo el crear tarea (#22) (#23)
    digo que esta prácticamente listo el crear tarea porque falta
    implementar lo de la base de datos además de organizar y borrar el
    ./pages/rubrica.js
    Tambien agregue cosas al readme, cosas  = contraseña de del correo
    
    Co-authored-by: Manuel <124811814+Daxtter@users.noreply.github.com>

---
+++ king-frontend/src/pages/Tarea.js
@@ -0,0 +1,161 @@
+import React, { PureComponent } from 'react'
+import CrearTareas from "../components/CrearTareas.js"
+import { Box, Button, IconButton} from '@mui/material';
+import Stepper from '@mui/joy/Stepper';
+import Tooltip from '@mui/joy/Tooltip';
+import Step, { stepClasses } from '@mui/joy/Step';
+import StepIndicator, { stepIndicatorClasses } from '@mui/joy/StepIndicator';
+import ShoppingCartRoundedIcon from '@mui/icons-material/ShoppingCartRounded';
+import AssignmentRoundedIcon from '@mui/icons-material/AssignmentRounded';
+import CheckCircleRoundedIcon from '@mui/icons-material/CheckCircleRounded';
+import ArrowBackRoundedIcon from '@mui/icons-material/ArrowBackRounded';
+import ArrowForwardRoundedIcon from '@mui/icons-material/ArrowForwardRounded';
+import { FlashOnRounded } from '@mui/icons-material';
+import ComponenteRubrica from "../components/ComponenteRubrica.js"
+import ConfirmacionDeTarea from '../components/ConfirmacionDeTarea.js';
+
+
+//Aqui se supone que se creará la tarea, para esto llamará a los componentes necesarios
+export const Tarea = ()=>{
+
+  const [indiceDeCreacion,setIndiceDeCreacion] = React.useState(0)
+  const [parameters, setParameters] = React.useState([])
+  const [parametrosTarea,setParametrosTarea] = React.useState({
+    taskName:'',
+    description:'',
+    studentList:''
+  })
+  //Esta funcion recibe la informacion que da el componente
+    const setTaskData = (data) => {
+        console.log("Datos recibidos desde CrearTareas:", data)
+        
+        
+      };
+    const setTaskDataRubrica = (data) => {
+      console.log("Datos recibidos desde rubrica:", data)
+      console.log("TaskName",data[0].taskName)
+      setParameters(data)
+      
+    };
+  
+    //Cambia el indice por lo que se cambia entre, creartarea, rubrica y finalizar(Confirmar)
+      const cambiarIndice = (numero)=>{
+        console.log(parametrosTarea)
+        console.log("Indice",indiceDeCreacion) //esto es como para comprobar cosas despues 
+        if (numero ===1){
+          if (indiceDeCreacion!==2) setIndiceDeCreacion(indiceDeCreacion+1)
+        }
+        else{
+          if (indiceDeCreacion!==0) setIndiceDeCreacion(indiceDeCreacion -1) 
+        }
+      }
+
+
+    return(
+        <Box sx={{  maxWidth: '700px', margin: '0 auto', padding: '2rem', border: '1px solid #ddd', borderRadius: '8px' }}>
+    
+          {indiceDeCreacion=== 0 &&(<CrearTareas parametrosTarea={parametrosTarea} setParametrosTarea={setParametrosTarea}/>)}
+          {indiceDeCreacion=== 1 &&(<ComponenteRubrica parameters={parameters} setParameters={setParameters}/>)}
+          {indiceDeCreacion === 2 && (<ConfirmacionDeTarea parametrosRubrica={ parameters} parametrosTarea={parametrosTarea}/>)}
+
+        <Box sx ={{minHeight: '10vh'}}/>
+
+        <Box sx={{display: 'flex', alignItems:'left',gap: 0.5}}>
+
+          {/* Botón izquierdo */}          
+          <IconButton onClick={()=>{cambiarIndice(0) }}  sx={{mr:'auto'}}>
+          <ArrowBackRoundedIcon/>
+          </IconButton>
+
+          {/* Botón derecho */}
+          { indiceDeCreacion !==2 &&(
+          <IconButton onClick={()=>{cambiarIndice(1) }} sx={{ml:'auto'}}>
+          <ArrowForwardRoundedIcon/>
+          </IconButton>)}
+          {
+              indiceDeCreacion ===2 &&(
+              <Button onClick={()=>{cambiarIndice(1) }} sx={{ml:'auto'}}>
+              Crear
+              </Button>)
+          }
+        </Box>
+        
+        
+        <Stepper
+      size="lg"
+      sx={{
+        width: '100%',
+        '--StepIndicator-size': '3rem',
+        '--Step-connectorInset': '1px',
+        mb:'auto',
+        [`& .${stepIndicatorClasses.root}`]: {
+          borderWidth: 4,
+        },
+        [`& .${stepClasses.root}::after`]: {
+          height: 4,  
+        },
+        [`& .${stepClasses.completed}`]: {
+          [`& .${stepIndicatorClasses.root}`]: {
+            borderColor: 'primary.300',
+            color: 'primary.300',
+          },
+          '&::after': {
+            bgcolor: 'primary.300',
+          },
+        },
+        [`& .${stepClasses.active}`]: {
+          [`& .${stepIndicatorClasses.root}`]: {
+            borderColor: 'currentColor',
+          },
+        },
+        [`& .${stepClasses.disabled} *`]: {
+          color: 'neutral.outlinedDisabledColor',
+        },
+      }}
+    >
+      <Tooltip title= "Crear tarea">
+      <Step
+        completed={ indiceDeCreacion>0 ? true :false}
+        active = {indiceDeCreacion ===0 ? true :false}
+        orientation="vertical"
+        indicator={
+          <StepIndicator variant="outlined" color="primary">
+            <ShoppingCartRoundedIcon />
+          </StepIndicator>
+        }
+      />
+      </Tooltip>
+      <Tooltip title= "Crear Rúbrica">
+      <Step
+        orientation="vertical"
+        completed={ indiceDeCreacion>1 ? true :false}
+        active = {indiceDeCreacion ===1 ? true :false}
+        disabled = {indiceDeCreacion < 1? true: false}
+        indicator={
+          <StepIndicator variant="outlined" color="primary">
+            <AssignmentRoundedIcon />
+          </StepIndicator>
+        }
+      />
+      </Tooltip>
+      <Tooltip title= " Finalizar">
+      <Step
+        orientation="vertical"
+        
+        completed={ indiceDeCreacion>2 ? true :false}
+        active = {indiceDeCreacion ===2 ? true :false}
+        disabled = {indiceDeCreacion < 2? true: false}
+        indicator={
+          <StepIndicator variant="outlined" color="primary">
+            <CheckCircleRoundedIcon />
+          </StepIndicator>
+        }
+      />
+    </Tooltip>
+    </Stepper>
+        </Box>
+    )
+}
+export default Tarea;
+
+
