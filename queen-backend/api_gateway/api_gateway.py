a1a127511d3a07d1ab11cb821b41c0280813cba8
XDurango2 <130610928+XDurango2@users.noreply.github.com>
2024-10-19 14:24:23 -0700

    avance de interconexión front-back (#25)
    se hicieron cambios al API gateway, al ia_response para hacer un stream
    de los datos que genera la respuesta de la IA con el fin que se reciban
    en el front a traves del gateway, Nota product owner: se resolvieron conflictos en el ai_response_test
    
    ---------
    
    Co-authored-by: Manuel <124811814+Daxtter@users.noreply.github.com>

---
+++ queen-backend/api_gateway/api_gateway.py
@@ -0,0 +1,62 @@
+from flask import Flask,request,jsonify
+from flask_cors import CORS
+import pathlib,sys
+# Importaciones absolutas ahora son posibles
+from authentication import google_auth 
+current_file=pathlib.Path(__file__)
+parent_dir=current_file.parent.parent
+#print(str(parent_dir))
+sys.path.append(str(parent_dir))
+from ia_function.ia_response import ia_response
+ia_response.run
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/login/google', methods=['POST'])
+def login_google():
+    return google_auth.login_google()
+
+@app.route('/tarea/rubrica', methods=['POST'])
+def get_rubric():
+    pass
+    #return process_rubric()
+
+# Ruta para enviar datos de la IA
+@app.route('/submit', methods=['POST'])
+def submit():
+    try:
+        data = request.json
+        # Enviamos los datos a la función submit() de ia_response
+        result = ia_response.submit(data)
+        return jsonify({"message": "Datos procesados exitosamente", "result": result}), 200
+    except Exception as e:
+        return jsonify({"error": f"Error al procesar los datos: {str(e)}"}), 500
+
+# Ruta para obtener la respuesta de la IA
+@app.route('/response', methods=['GET'])
+def get_response():
+    try:
+        result = ia_response.Response()
+        return jsonify({"response": result}), 200
+    except Exception as e:
+        return jsonify({"error": f"Error al obtener la respuesta: {str(e)}"}), 500
+
+# Ruta para enviar los criterios de la IA
+@app.route('/submit_criteria', methods=['POST'])
+def submit_criteria():
+    try:
+        criteria_data = request.json
+        ia_response.submit_criteria(criteria_data)
+        return jsonify({"message": "Criterios enviados exitosamente"}), 200
+    except Exception as e:
+        return jsonify({"error": f"Error al enviar los criterios: {str(e)}"}), 500
+
+
+@app.after_request
+def add_header(response):
+    response.headers['Cross-Origin-Opener-Policy'] = 'same-origin'
+    response.headers['Cross-Origin-Embedder-Policy'] = 'require-corp'
+    return response
+
+if __name__ == '__main__':
+    app.run(debug=True)
