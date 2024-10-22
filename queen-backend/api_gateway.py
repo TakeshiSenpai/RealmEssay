75b03cf811ae2771486ffdc83333c7fa0f269628
Leonardo <83844690+LeonardoLarranaga@users.noreply.github.com>
2024-10-22 15:05:34 -0700

    Modificación de `auto_run` para que funcione estando dentro o fuera de la carpeta de `queen-backend` (#47)
    Junto con un poquito de limpieza de código.
    
    ---------
    
    Co-authored-by: Leonardo Larrañaga <leonardolarranaga@icloud.com>

--- queen-backend/api_gateway.py
+++ queen-backend/api_gateway.py
@@ -1,12 +1,6 @@
-from flask import Flask,request,jsonify,Response
-from flask_cors import CORS
-import pathlib,sys
 import requests
-# Importaciones absolutas ahora son posibles
-current_file=pathlib.Path(__file__)
-parent_dir=current_file.parent.parent
-#print(str(parent_dir))
-sys.path.append(str(parent_dir))
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
@@ -31,49 +25,44 @@ def get_rubric():
 
     return jsonify(response.json()), 200 
 
-
-
-"""
-# Ruta para enviar datos de la IA
-@app.route('/submit', methods=['POST'])
-def submit():
-    try:
-        data = request.json
-        # Enviamos los datos a la función submit() de ia_response
-        result = ia_response.submit(data)
-        return jsonify({"message": "Datos procesados exitosamente", "result": result}), 200
-    except Exception as e:
-        return jsonify({"error": f"Error al procesar los datos: {str(e)}"}), 500
-
-
-# Ruta para obtener la respuesta de la IA
-@app.route('/response', methods=['GET'])
-def get_response():
-    try:
-        # Procesar la respuesta como un stream de datos
-        def stream_response():
-            for fragment in ia_response.process_response():
-                yield f"data: {fragment}\n\n"  # Enviar cada fragmento al cliente progresivamente
-                import sys
-                sys.stdout.flush()  # Asegurarnos de que se "flushee" el buffer
-
-        # Retornamos el stream usando la función generadora
-        return Response(stream_response(), content_type='text/event-stream')
-
-    except Exception as e:
-        return jsonify({"error": f"Error al obtener la respuesta: {str(e)}"}), 500
-
-# Ruta para enviar los criterios de la IA
-@app.route('/submit_criteria', methods=['POST'])
-def submit_criteria():
-    try:
-        criteria_data = request.json
-        ia_response.submit_criteria(criteria_data)
-        return jsonify({"message": "Criterios enviados exitosamente"}), 200
-    except Exception as e:
-        return jsonify({"error": f"Error al enviar los criterios: {str(e)}"}), 500
-
-"""
+# # Ruta para enviar datos de la IA
+# @app.route('/submit', methods=['POST'])
+# def submit():
+#     try:
+#         data = request.json
+#         # Enviamos los datos a la función submit() de ia_response
+#         result = ia_response.submit(data)
+#         return jsonify({"message": "Datos procesados exitosamente", "result": result}), 200
+#     except Exception as e:
+#         return jsonify({"error": f"Error al procesar los datos: {str(e)}"}), 500
+#
+#
+# # Ruta para obtener la respuesta de la IA
+# @app.route('/response', methods=['GET'])
+# def get_response():
+#     try:
+#         # Procesar la respuesta como un stream de datos
+#         def stream_response():
+#             for fragment in ia_response.process_response():
+#                 yield f"data: {fragment}\n\n"  # Enviar cada fragmento al cliente progresivamente
+#                 import sys
+#                 sys.stdout.flush()  # Asegurarnos de que se "flushee" el buffer
+#
+#         # Retornamos el stream usando la función generadora
+#         return Response(stream_response(), content_type='text/event-stream')
+#
+#     except Exception as e:
+#         return jsonify({"error": f"Error al obtener la respuesta: {str(e)}"}), 500
+#
+# # Ruta para enviar los criterios de la IA
+# @app.route('/submit_criteria', methods=['POST'])
+# def submit_criteria():
+#     try:
+#         criteria_data = request.json
+#         ia_response.submit_criteria(criteria_data)
+#         return jsonify({"message": "Criterios enviados exitosamente"}), 200
+#     except Exception as e:
+#         return jsonify({"error": f"Error al enviar los criterios: {str(e)}"}), 500
 
 @app.after_request
 def add_header(response):
