eef6308449864f7de352c78df1050e89ed035e45
Kadir Perez <perez.kadir@uabc.edu.mx>
2024-10-22 02:58:35 -0700

    Reestructuracion completa del backend

--- queen-backend/server_authentication/google_auth.py
+++ queen-backend/server_authentication/google_auth.py
@@ -1,8 +1,13 @@
-from flask import request, jsonify
+from flask import Flask,request,jsonify
+from flask_cors import CORS
 from google.oauth2 import id_token
 from google.auth.transport import requests
 import jwt
 import datetime
+
+app = Flask(__name__)
+CORS(app)
+
 
 # Client ID de Google
 GOOGLE_CLIENT_ID = "496696206304-fsqr77k8ao63rv6tuskh5lu4ph5p4fo3.apps.googleusercontent.com"
@@ -18,6 +23,7 @@ def generate_jwt(user_info):
     }
     return jwt.encode(payload, SECRET_KEY, algorithm='HS256')
 
+@app.route('/login/google', methods=['POST'])
 def login_google():
     token = request.json.get('token')
     try:
@@ -31,3 +37,12 @@ def login_google():
         return jsonify({'success': True, 'user_info': user_info, 'token': jwt_token}), 200
     except ValueError:
         return jsonify({'success': False, 'message': 'Invalid token'}), 400
+    
+@app.after_request
+def add_header(response):
+    response.headers['Cross-Origin-Opener-Policy'] = 'same-origin'
+    response.headers['Cross-Origin-Embedder-Policy'] = 'require-corp'
+    return response
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=2001)
