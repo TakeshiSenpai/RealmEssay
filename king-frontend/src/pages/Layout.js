f367bb9852e344ccdadbd95df72740d5b1d17adc
Manuel <lmnunez2003@gmail.com>
2024-10-21 18:47:50 -0700

    Ahora ya no esta la rubrica en layout

--- king-frontend/src/pages/Layout.js
+++ king-frontend/src/pages/Layout.js
@@ -171,13 +171,7 @@ const Layout = ({theme, setTheme, isAuto}) => {
                         mostrarContenidoEnSidebarListItem()
 
                     }
-                    <ListItem>
-                        <ListItemButton component={Link} to="/Rubrica">
-                            <EditNote/>
-                            Rúbrica
-                        </ListItemButton>
-                    </ListItem>
-                </List>
+                    </List>
 
                 <Box
                     sx={{
