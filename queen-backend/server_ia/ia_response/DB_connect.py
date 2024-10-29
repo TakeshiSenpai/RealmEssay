7b25c390683ac30323e86f6a557ae398fc2da798
XDurango2 <130610928+XDurango2@users.noreply.github.com>
2024-10-28 17:35:45 -0700

    Hotfix_1 (#62)
    tiene un hotfix para un problema que se tiene al momento de interactuar
    un interactions.json, causa un problema y hace que ia.py genere codigos
    500

--- queen-backend/server_ia/ia_response/DB_connect.py
+++ queen-backend/server_ia/ia_response/DB_connect.py
@@ -2,19 +2,22 @@ import sys,pathlib,json
 sys.path.append(str(pathlib.Path(__file__).parent.parent.parent.parent.resolve()))
 from database.ChatDB import Chat
 
-INTERACTIONS_FILE =pathlib.Path('queen-backend/server_ia/ia_response/interactions.json').resolve()
+INTERACTIONS_FILE =pathlib.Path('queen-backend/server_ia/ia_response/user_interactions/interactions.json').resolve()
 
 
 def test_post():
     Chat.post.crear_chat_desde_json(INTERACTIONS_FILE)
 
-
 def test_put():
     #Chat.put.actualizar_chat_desde_json()
     pass
+
 def test_get():
     Chat.get.exportar_chats_a_json(INTERACTIONS_FILE)
 
 def test_delete():
     #Chat.delete.eliminar_chat()
     pass
+
+
+test_post()
