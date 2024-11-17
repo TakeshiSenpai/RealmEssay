b593a6ec98b1f431985664fa78bb67f6cc8adabf
XDurango2 <130610928+XDurango2@users.noreply.github.com>
2024-11-16 19:07:31 -0800

    correcciones del stream de datos
    ya se envia correctamente el stream de datos de la IA hacia el frontend, tambien se incluye un script de python para pruebas automatizadas

--- queen-backend/server_ia/ia_response/response_tests/ia_questions_test_output.py
+++ queen-backend/server_ia/ia_response/response_tests/ia_questions_test_output.py
@@ -13,14 +13,16 @@ inicio=time.time()
 # Hacer la solicitud POST con stream habilitado
 response = requests.post(url, json=data, stream=True)
 
+#
 # Verificar si la solicitud fue exitosa
 if response.status_code == 200:
     print("Recibiendo la respuesta en fragmentos:")
-    
-   for text in response:
-
+    for chunk in response.iter_content(chunk_size=None, decode_unicode=True):
+        # Imprime cada fragmento del stream
+        print(chunk, end='', flush=True)
 else:
     print(f"Error: {response.status_code} - {response.text}")
+
 final=time.time()
 tiempo_exec=final-inicio
 print(f"tiempo de ejecucion {tiempo_exec} seg")
