929212c1876bbbc6d07ede9dd0eadf273798e12c
Leonardo Larrañaga <leonardolarranaga@icloud.com>
2024-11-18 20:43:57 -0800

    Fixed bug donde no se puede acceder a crear tarea desde home

--- king-frontend/src/components/Home/Teacher/TeacherHome.jsx
+++ king-frontend/src/components/Home/Teacher/TeacherHome.jsx
@@ -3,16 +3,22 @@ import {Button} from "@mui/material"
 import {Grading} from "@mui/icons-material"
 import Typography from "@mui/material/Typography"
 import {useNavigate} from "react-router-dom"
+import {useIsStudent} from "../../IsStudentProvider";
 
 const TeacherHome = () => {
     const navigate = useNavigate()
+    const {isStudent, setIsStudent} = useIsStudent()
+
     return (
         <Box>
             <Box display="flex" justifyContent="center">
                 <Button
                     variant="contained"
                     endIcon={<Grading/>}
-                    onClick={() => navigate("/essays/createhomework")}
+                    onClick={() => {
+                        setIsStudent(true)
+                        navigate("/essays/createhomework")
+                    }}
                 >
                     <Typography fontWeight="bold" textTransform="none">
                         Crear nueva tarea
